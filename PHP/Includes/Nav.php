4036f48e5139c7db412660c175cc2a2379eea6fb
Resett13 <pietrogoncalvesaraujo1301@gmail.com>
2024-10-25 20:06:55 -0300

    commit1

---
+++ PHP/Includes/Nav.php
@@ -0,0 +1,21 @@
+<link rel="stylesheet" href="/Style/Nav.css">
+</head>
+<body>
+    <nav>
+        <div class="nav_itens">
+        <div class="texto">
+            <h3>CALENDÁRIO ESCOLAR - 2024</h3>
+            <br>
+            <h3>(2024 - Etec da Zona Leste - sede)</h3>
+            <br>
+            <h5>Cód. Inst:211 Unidade:Etec Zona Leste Cidade: São Paulo</h5>
+
+        </div>
+       <div class="Logo">
+       <img src="/Imagens/etec_zona_leste_logo.png">
+       <img src="/Imagens/gov-sp-vertical.png">
+       </div>
+    </div>
+    </nav>
+</body>
+</html>
---
+++ PHP/Index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/Style/Index.css">
+    <title>Index</title>
+</head>
+<body>
+    <header>
+        <?php include("./Includes/Nav.php"); ?>
+    </header>
+    <main>
+        <h1>Bem-vindo ao nosso site!</h1>
+        <p>Aqui está o conteúdo principal.</p>
+    </main>
+</body>
+</html>
