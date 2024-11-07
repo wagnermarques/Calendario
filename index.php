f1de971adc7e2fe9ace4ef1426e105bef9d9b45a
wagnermarques <wagnerdocri@gmail.com>
2024-11-07 18:26:26 -0300

    adicionei index.php na raiz do projeto

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <meta http-equiv="refresh" content="0; url=https://green-smoke-cf49a2c3fc5146409064d11e9cbf5270.azurewebsites.net/PHP/">
+</head>
+<body>
+    
+</body>
+</html>
